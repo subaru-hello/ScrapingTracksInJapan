3c64884eb939a432153ab44638a7d973a0dc74d6
subaru-hello <octosubaru0926@gmail.com>
2022-10-25 19:22:31 +0900

    output the datas to csv after fetching track and name from url

--- track_in_kanagawa.py
+++ track_in_kanagawa.py
@@ -1,19 +1,28 @@
 import requests
-url='https://www.homemate-research-athletic-field.com/14/list/'
-
+import pandas as pd
 from bs4 import BeautifulSoup
+
+url='https://www.homemate-research-athletic-field.com/14/list/'
 
 response = requests.get(url)
 results = []
+other_results_virtual = []
+other_results = []
 soup = BeautifulSoup(response.text, 'html.parser')
 for element in soup.findAll(attrs={'class': 'fa_ttl'}):
     name = element.find('a')
-    results.append(name.text)
+    if name not in results:
+        results.append(name.text)
+for b in soup.findAll(attrs={'class': 'fa_address'}):
+    name2 = b.contents[3]
+    other_results_virtual.append(name2.text.strip('\t/\n'))
 
-for el in soup.findAll(attrs={'class': 'fa_address'}):
-    address = el.find('span')
-    results.append(address.text)
+other_result_length = len(other_results_virtual)
+
+for i in range(0, other_result_length):
+    if i % 2 == 0:
+        other_results.append(other_results_virtual[i])
 
 
-
-print(results)
+df = pd.DataFrame({'Names': results, 'Address': other_results})
+df.to_csv('tracks.csv', index=False, encoding='utf-8')
