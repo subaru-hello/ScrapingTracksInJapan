a75ee72b819c5c71e1ad71db100bbb614a77a3ca
subaru-hello <octosubaru0926@gmail.com>
2022-10-26 10:13:39 +0900

    change directory

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-url='https://oxylabs.io/blog'
-response = requests.get(url)
-
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.title)
-
--- test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import chromedriver_binary
-driver = webdriver.Chrome()         
-driver.get('https://oxylabs.io/blog')
-
-blog_titles = driver.get_elements_by_css_selector(' h2.blog-card__content-title')
-for title in blog_tiles:
-    print(title.text)
-driver.quit()
