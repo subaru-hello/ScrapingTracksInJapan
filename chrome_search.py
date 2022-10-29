5ffa644f41e2b71149a0ed71941eb6cd61e9aa0a
subaru-hello <octosubaru0926@gmail.com>
2022-10-29 14:02:16 +0900

    chrome driver

---
+++ chrome_search.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import chromedriver_binary
+
+driver = webdriver.Chrome()         
+driver.get('https://www.city.kawasaki.jp/nakahara/page/0000088519.html')
+
+results = []
+content = driver.page_source
+soup = BeautifulSoup(content, 'html.parser')
+for element in soup.findAll(attrs={'class': 'mol_attachfileblock'}):
+    name = element.find('a')
+    print(name)
+
+driver.quit
+
+
+
