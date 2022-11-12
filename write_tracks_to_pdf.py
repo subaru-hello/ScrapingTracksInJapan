db4d5786aa36aa4ba26dc0b94cef82fbbc6ed4bc
subaru-hello <octosubaru0926@gmail.com>
2022-11-12 19:39:28 +0900

    取得したpdfをCSVへ出力

--- write_tracks_to_pdf.py
+++ write_tracks_to_pdf.py
@@ -1,36 +1,18 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import chromedriver_binary
-import re
-import urllib
+import csv
 import pandas as pd
 import tabula
 import os
 
-base_url = 'https://www.city.kawasaki.jp/nakahara'
-driver = webdriver.Chrome()
-driver.get("https://www.city.kawasaki.jp/nakahara/page/0000088519.html")
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
+file_name_list = os.listdir("./track_pdfs")
 
-
-save_dir = 'track_pdfs'
-if save_dir not in os.listdir("./"):
-    os.mkdir(save_dir)
-
-for element in soup.findAll(attrs={"class": "mol_attachfileblock"}):
-    name = element.find("a")
-    pdf_relative_path = name.get('href')
-    pdf_path = re.sub(r'^.', base_url, pdf_relative_path)
-    file_name = pdf_relative_path.split("/")[-1]
-    urllib.request.urlretrieve(pdf_path, os.path.join(save_dir, file_name))
-
-
-# df = tabula.read_pdf(pdf_file_name, pages = '1', multiple_tables = False)[0]
-#print(pdf_path)
-
-
-# base url
-# https://www.city.kawasaki.jp/nakahara/
-driver.quit
+for pdf_name in file_name_list:
+  pdf = './track_pdfs/' + pdf_name
+  df = tabula.read_pdf(pdf, lattice=True, pages ='all')
+  csv_path = "./csvs/{0}.csv".format(pdf_name)
+  
+  if type(df) is list:
+    for deep_df in df:
+      deep_df.to_csv(csv_path, index=False, encoding="utf-8")
+  else:
+    df.to_csv(csv_path, index=False, encoding="utf-8")
