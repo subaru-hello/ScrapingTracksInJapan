3ce74de8a4f156935230a693e9677a6b97c255d2
subaru-hello <octosubaru0926@gmail.com>
2022-11-19 11:57:43 +0900

    rename Unnnamed: 0

--- write_tracks_to_pdf.py
+++ write_tracks_to_pdf.py
@@ -6,13 +6,23 @@ import os
 
 file_name_list = os.listdir("./track_pdfs")
 
-for pdf_name in file_name_list:
-  pdf = './track_pdfs/' + pdf_name
-  df = tabula.read_pdf(pdf, lattice=True, pages ='all')
-  csv_path = "./csvs/{0}.csv".format(pdf_name)
-  
-  if type(df) is list:
-    for deep_df in df:
-      deep_df.to_csv(csv_path, index=False, encoding="utf-8")
-  else:
-    df.to_csv(csv_path, index=False, encoding="utf-8")
+def rename_index(df):
+    return df.rename(columns={"個人利用": "個人利用a.m", "Unnamed: 0": "個人利用p.m"})
+
+def main():
+  for pdf_name in file_name_list:
+    pdf = './track_pdfs/' + pdf_name
+    df = tabula.read_pdf(pdf, lattice=True, pages ='all')
+    csv_path = "./csvs/{0}.csv".format(pdf_name)
+
+    if type(df) is list:
+      for deep_df in df:
+        renamed_df = rename_index(deep_df)
+        renamed_df.to_csv(csv_path, index=False, encoding="utf-8")
+        print(renamed_df)
+    else:
+      renamed_df = rename_index(df)
+      renamed_df.to_csv(csv_path, index=False, encoding="utf-8")
+      print(renamed_df)
+
+main()
